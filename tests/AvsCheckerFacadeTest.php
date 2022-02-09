0330ba34cf1d665de7ce2544cccad71aab1a0788
PatrickJunod <patrick@junod.ch>
2022-02-09 11:53:48 +0100

    Add: Tests

--- tests/AvsCheckerFacadeTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use PatrickJunod\AvsChecker\Facades\AvsChecker;
-
-it('can instantiate a new AVS Checker as Facade', function () {
-    expect(AvsChecker::isValid('test'))->toBeFalse();
-});
